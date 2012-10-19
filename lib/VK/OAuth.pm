71b34f9d2ccc356b00cbe886f7001f404f39c59d
Alexander Babenko <foxcool333@gmail.com>
2012-10-19 06:48:54 +0000

    Import of FOXCOOL/VK-OAuth-0.02 from CPAN.
    gitpan-cpan-distribution: VK-OAuth
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         FOXCOOL/VK-OAuth-0.02.tar.gz
    gitpan-cpan-author:       FOXCOOL
    gitpan-cpan-maturity:     released

--- lib/VK/OAuth.pm
+++ lib/VK/OAuth.pm
@@ -9,7 +9,7 @@ require LWP::UserAgent;
 require JSON;
 require Carp;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 sub new {
@@ -28,12 +28,13 @@ sub new {
 sub authorize {
     my ($self, $params) = @_;
 
+    my %params = %$params if $params && %$params;
     my $url = URI->new('https://oauth.vk.com/authorize');
     $url->query_form(
         client_id     => $self->{app_id},
         response_type => 'code',
         redirect_uri  => $self->{postback},
-        $params && %$params ? %$params : undef,
+        %params,
     );
     return $url;
 }
@@ -58,14 +59,15 @@ sub request_access_token {
 }
 
 sub request {
-    my ($self, $method, $params, $access_token) = @_;
+    my ( $self, $method, $access_token, $params ) = @_;
 
     Carp::croak("method and access_token required for this action")
       unless ($method && $access_token);
+    my %params = %$params if $params && %$params;
     my $url = URI->new('https://api.vk.com/method/' . $method);
     $url->query_form(
         access_token => $access_token,
-        $params && %$params ? %$params : undef,
+        %params,
     );
     my $response = $self->{ua}->get($url);
     return 0 unless $response->is_success;
@@ -87,12 +89,25 @@ VK::OAuth - OAuth authorization on your site with VK API
 
 =head1 SYNOPSIS
 
-  my $object = VK::OAuth->new(
+  my $vk = VK::OAuth->new(
       app_id     => 'YOUR APP ID',
       secret     => 'YOUR APP SECRET',
       postback   => 'POSTBACK URL',
   );
-  
+  my $url = $vk->authorize( {option => 'value'} );
+  # Your web app redirect method.
+  $self->redirect($url);
+  # Get access_token.
+  my $access = $vk->request_access_token($args->{code});
+  # Get user profile data.
+  my $profile_data = $vk->request(
+      'users.get',
+      $access_token,
+      {
+	    fields => "uid,first_name,last_name,nickname,screen_name,sex,bdate,city,country",
+        uids   => $user_id,
+      }
+  );  
 
 
 =head1 DESCRIPTION
@@ -129,6 +144,8 @@ Method returns URI object.
 
 =head2 request_access_token
 
+  my $access = $vk->request_access_token($args->{code});
+
 This method gets access token from VK API.
 
 =head3 Options
@@ -140,6 +157,15 @@ code - returned in redirected get request from authorize API method.
 Method returns HASH object with keys $obj->{user_id}, $obj->{access_token}, $obj->{expires_in}.
 
 =head2 request
+
+  my $profile_data = $vk->request(
+      'users.get',
+      $access_token,
+      {
+	    fields => "uid,first_name,last_name,nickname,screen_name,sex,bdate,city,country",
+        uids   => $user_id,
+      }
+  );
 
 This method sends requests to VK API.
 
